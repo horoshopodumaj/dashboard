19544a62694db3d9a8bd7669c8a9e0e356c87a4c
Diana Murtazina <murka8831@yandex.ru>
2024-01-12 23:34:54 +0300

    added routes, pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/users",
+            element: <Users />,
+        },
+        {
+            path: "/products",
+            element: <Products />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
