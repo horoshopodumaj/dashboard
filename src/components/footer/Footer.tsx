b40a0cb6ccd5ea7e50580366d57d7d56181b84e3
Diana Murtazina <murka8831@yandex.ru>
2024-01-13 17:11:25 +0300

    footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-    return <div className="footer">Footer</div>;
+    return (
+        <div className="footer">
+            <span>dashboard</span>
+            <span>© dashboard</span>
+        </div>
+    );
 };
 
 export default Footer;
