9da393e93c3010afb1c78e639a1cf036b8407b0f
Diana Murtazina <murka8831@yandex.ru>
2024-01-13 16:15:42 +0300

    added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Products from "./pages/products/Products";
 import Users from "./pages/users/Users";
+import "./styles/global.scss";
 
 function App() {
+    const Layout = () => {
+        return (
+            <div className="main">
+                <Navbar />
+                <div className="container">
+                    <div className="menuContainer">
+                        <Menu />
+                    </div>
+                    <div className="contentContainer">
+                        <Outlet />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    };
+
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Home />,
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "/users",
+                    element: <Users />,
+                },
+                {
+                    path: "/products",
+                    element: <Products />,
+                },
+            ],
         },
         {
-            path: "/users",
-            element: <Users />,
-        },
-        {
-            path: "/products",
-            element: <Products />,
+            path: "/login",
+            element: <Login />,
         },
     ]);
 
