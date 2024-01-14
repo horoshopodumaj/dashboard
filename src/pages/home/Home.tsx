9aa9ef99d357eda890e90ba21dc7e91643673509
Diana Murtazina <murka8831@yandex.ru>
2024-01-14 21:20:39 +0300

    added bigChartBox component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
 import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
@@ -30,7 +31,9 @@ const Home = () => {
                 <ChartBox {...chartBoxProduct} />
             </div>
 
-            <div className="box box7">box7</div>
+            <div className="box box7">
+                <BigChartBox />
+            </div>
             <div className="box box8">
                 <BarChartBox {...barChartBoxVisit} />
             </div>
