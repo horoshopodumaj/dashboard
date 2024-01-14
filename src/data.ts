c87ff07bf5f6091dc5a0771a33f7142767ad9734
Diana Murtazina <murka8831@yandex.ru>
2024-01-14 17:22:46 +0300

    added topbox comnonent

--- src/data.ts
+++ src/data.ts
@@ -114,3 +114,55 @@ export const menu = [
         ],
     },
 ];
+
+export const topDealUsers = [
+    {
+        id: 1,
+        img: "https://freerangestock.com:443/sample/3037/meggie-bw.jpg",
+        username: "Elva McDonald",
+        email: "elva@gmail.com",
+        amount: "3.668",
+    },
+    {
+        id: 2,
+        img: "https://freerangestock.com:443/sample/114814/a-caucasian-woman-posing-with-a-smile.jpg",
+        username: "Linnie Nelson",
+        email: "linnie@gmail.com",
+        amount: "3.256",
+    },
+    {
+        id: 3,
+        img: "https://freerangestock.com:443/sample/138996/young-man-in-suspenders-with-whiskey-glass--looking-away.jpg",
+        username: "Brent Reeves",
+        email: "brent@gmail.com",
+        amount: "2.998",
+    },
+    {
+        id: 4,
+        img: "https://freerangestock.com:443/sample/142300/business-woman-working-at-a-desk.jpg",
+        username: "Adeline Watson",
+        email: "adeline@gmail.com",
+        amount: "2.512",
+    },
+    {
+        id: 5,
+        img: "https://freerangestock.com:443/sample/141820/happy-office-worker-working-on-a-laptop-with-paper-notes.jpg",
+        username: "Juan Harrington",
+        email: "juan@gmail.com",
+        amount: "2.134",
+    },
+    {
+        id: 6,
+        img: "https://freerangestock.com:443/sample/138822/young-businessman-in-blue-three-piece-suit.jpg",
+        username: "Augusta McGee",
+        email: "augusta@gmail.com",
+        amount: "1.932",
+    },
+    {
+        id: 7,
+        img: "https://freerangestock.com:443/sample/138138/woman-working-on-laptop-computer.jpg",
+        username: "Angel Thomas",
+        email: "angel@gmail.com",
+        amount: "1.560",
+    },
+];
